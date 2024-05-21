79a5f55fbfd48fd3e634812daecb9c4ac664843f
Yushan Jayasekara <88585505+vidurangaushan@users.noreply.github.com>
2024-05-21 15:06:12 +0530

    Update __init__.py

--- Backend/application/__init__.py
+++ Backend/application/__init__.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:5173'])
-app.config["MONGO_URI"] = "mongodb+srv://superushan17521:Hj8k9zVx5PBUEWai@cluster0.l09ymk4.mongodb.net/Eduflex"
+app.config["MONGO_URI"] = ""
 
 # mongodb database
 mongodb_client = PyMongo(app)
