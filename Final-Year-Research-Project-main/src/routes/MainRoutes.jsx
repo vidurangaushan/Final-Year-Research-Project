872165adfb6baf57ddcfa0d250fb6f3b115a8eb8
Yushan Viduranga <88585505+vidurangaushan@users.noreply.github.com>
2024-05-21 15:35:23 +0530

    Update frontend

--- Final-Year-Research-Project-main/src/routes/MainRoutes.jsx
+++ Final-Year-Research-Project-main/src/routes/MainRoutes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import SelectionPage from "../pages/Selection/SelectionPage";
+import SelectionPage_KQ from "../pages/Selection/SelectionPage_KQ";
 import MathsForm from "../pages/forms/Maths/MathsForm";
 import HomePage from "../pages/Home/Home";
 import EnglishForm from "../pages/forms/English/EnglishForm";
@@ -10,12 +10,13 @@ import { Reports } from "../pages/Reports";
 import { Maths } from "../pages/Maths";
 import { Science } from "../pages/Science";
 import { English } from "../pages/English";
+import { StudentReport } from "../pages/Assesment/QuizReport.jsx";
 
 export default function MainRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/selection" element={<SelectionPage /> }/>
+        <Route path="/Quizselection" element={<SelectionPage_KQ /> }/>
         <Route path="/MathsQuiz" element={<MathsForm /> }/>
         <Route path="/EnglishQuiz" element={<EnglishForm /> }/>
         <Route path="/ScienceQuiz" element={<ScienceForm /> }/>
@@ -25,7 +26,8 @@ export default function MainRoutes() {
         <Route path="/english" element={<English />}/>
         
         <Route path="/" element={<HomePage /> }/>
-        <Route path="/Reports" element={<Reports /> }/>
+        <Route path="/quizprofile" element={<StudentReport /> }/>
+        <Route path="/QuizReports" element={<Reports /> }/>
       </Routes>
     </BrowserRouter>
   );
