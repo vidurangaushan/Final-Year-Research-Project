85418054023ff8f623ed22c133250cab11e3e0d6
Yushan Viduranga <88585505+vidurangaushan@users.noreply.github.com>
2024-05-21 15:29:55 +0530

    Update Frontend

--- Final-Year-Research-Project-main/src/Content/Text.jsx
+++ Final-Year-Research-Project-main/src/Content/Text.jsx
@@ -3,7 +3,7 @@ export const homeContent = {
     aboutUs: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
     
     bt1Name : "Quiz",
-    btn1Navigation : "/selection",
+    btn1Navigation : "/Quizselection",
     bt2Name : "Story",
     btn2Navigation : "/FamilyQuiz",
 
